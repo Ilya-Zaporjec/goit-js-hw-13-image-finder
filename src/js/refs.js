d5b9bafabc45d72c391160e3cf395a9de924f85c
Ilya-Zaporjec <ilya.zaporojec@gmail.com>
2021-08-26 20:49:14 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,8 +1,9 @@
-export default {
-  refs,
-};
 const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('gallery'),
   loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
+};
+
+export default {
+  refs,
 };
