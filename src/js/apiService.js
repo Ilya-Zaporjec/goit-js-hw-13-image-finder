bb15a52939771db728281de4593964f76789b4bb
Ilya-Zaporjec <ilya.zaporojec@gmail.com>
2021-08-27 14:56:06 +0300

    new ApiFn

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -3,15 +3,15 @@ export default class NewApiService {
     this.page = 1;
     this.searchQuery = '';
   }
-
   fethcArticles = async () => {
     const baseUrl = 'https://pixabay.com/api/';
     const key = '23071617-4384eb8116da3def56182313f';
-    const respons = `${baseUrl}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${key}`;
+    const fetchUrl = `${baseUrl}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${key}`;
     this.incrementPage();
-    return fetch(respons).then(respons => respons.json());
+    const response = await fetch(fetchUrl);
+    const { hits } = await response.json();
+    return hits;
   };
-
   get query() {
     return this.searchQuery;
   }
--- test.js
+++ test.js
@@ -6,3 +6,31 @@
 //   this.incrementPage();
 //   return string.hits;
 // };
+
+// export default class NewApiService {
+//   constructor() {
+//     this.page = 1;
+//     this.searchQuery = '';
+//   }
+
+//   fethcArticles = async () => {
+//     const baseUrl = 'https://pixabay.com/api/';
+//     const key = '23071617-4384eb8116da3def56182313f';
+//     const respons = `${baseUrl}?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=12&key=${key}`;
+//     this.incrementPage();
+//     return fetch(respons).then(respons => respons.json());
+//   };
+
+//   get query() {
+//     return this.searchQuery;
+//   }
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+//   incrementPage() {
+//     this.page += 1;
+//   }
+//   resetPage() {
+//     this.page = 1;
+//   }
+// }
